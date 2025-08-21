50646b2576477c74edc819a87ae3d6b369e67c63
David-396 <david.eini12@gmail.com>
2025-08-22 00:22:59 +0300

    latest in docker img

--- scripts/scripts.bat
+++ scripts/scripts.bat
@@ -1,6 +1,6 @@
-docker build -t david4554545/hostile-tweets-identifier:1.0 .
+docker build -t david4554545/hostile-tweets-identifier:latest .
 docker run  --name hostile_tweets_identifier -p 8080:8080 -d david4554545/hostile-tweets-identifier:1.0
-docker push david4554545/hostile-tweets-identifier:1.0
+docker push david4554545/hostile-tweets-identifier:latest
 
 oc delete deployment hostile-tweets-identifier
 oc delete secret hostile-tweets-secret
